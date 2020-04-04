7f5090093e23e2c1dc3c4e463fefb58827c4e574
Nikita Mitin <werrty859@gmail.com>
2020-04-05 02:58:30 +0500

    CLUTH_1

--- Code/Scr/main.c
+++ Code/Scr/main.c
@@ -9,6 +9,27 @@
 #include "main.h"
 /*********************************************************************/
 
+/****************************** DEFINES ******************************/
+#define klapan_1_pin 1
+#define klapan_2_pin 2
+#define klapan_3_pin 3
+#define klapan_4_pin 4
+
+#define inertDelay 100 //ms
+#define cluthDelay 100 //ms
+
+#define NumOfGear 4
+
+#define klapan_1_ON GPIOC->ODR |= (1 << klapan_1_pin)
+#define klapan_2_ON GPIOC->ODR |= (1 << klapan_2_pin)
+#define klapan_3_ON GPIOC->ODR |= (1 << klapan_3_pin)
+#define klapan_4_ON GPIOC->ODR |= (1 << klapan_4_pin)
+#define klapan_1_OFF GPIOC->ODR &= ~(1 << klapan_1_pin)
+#define klapan_2_OFF GPIOC->ODR &= ~(1 << klapan_2_pin)
+#define klapan_3_OFF GPIOC->ODR &= ~(1 << klapan_3_pin)
+#define klapan_4_OFF GPIOC->ODR &= ~(1 << klapan_4_pin)
+
+/*********************************************************************/
 uint64_t countT = 0;
 
 int main(void){
@@ -17,17 +38,12 @@ int main(void){
 	TIM_Init();
 	RCC_Init();
 	PWM_Init();
-	CRC_Init();
 
-	uint16_t i = 0;
-	uint32_t crc = 0;
+
+
 	while(1){
-		TIM3->CCR3 = i;
-		i = (i < 80) ? i + 1 : 0;
-		DelayMillis(40);
-		CRC->DR = CRC_DR_DR;
-		CRC->DR = __REV(123456789);
-		crc = __REV(CRC->DR);
+
+
 
 	}
 }
@@ -36,7 +52,7 @@ int main(void){
 /***************************** IRQHandlers **************************/
 void TIM14_IRQHandler(void){
 	countT++;
-	TIM14->SR &= ~TIM_SR_UIF;				//сброс флага прерываний
+	TIM14->SR &= ~TIM_SR_UIF;										//сброс флага прерываний
 }
 
 /********************************************************************/
@@ -49,10 +65,37 @@ void DelayMillis(uint32_t time){
 	TIM14->CR1  |= TIM_CR1_CEN;
 	while(countT < time){}
 	countT = 0;
-	TIM14->DIER &= ~TIM_DIER_UIE;								// комбинация команд запускает таймер, запись 0 приведет к останавке
+	TIM14->DIER &= ~TIM_DIER_UIE;									// комбинация команд запускает таймер, запись 0 приведет к останавке
 	TIM14->CR1  &= ~TIM_CR1_CEN;
 }
 
+void GearUP(){
+	klapan_3_ON;
+	DelayMillis(cluthDelay);
+	klapan_3_OFF;
 
+	klapan_1_ON;
+	DelayMillis(inertDelay);
+	klapan_1_OFF;
+	DelayMillis(inertDelay);
 
+	klapan_4_ON;
+	DelayMillis(cluthDelay);
+	klapan_4_OFF;
+}
+
+void GearDOWN(){
+	klapan_3_ON;
+	DelayMillis(cluthDelay);
+	klapan_3_OFF;
+
+	klapan_1_ON;
+	DelayMillis(inertDelay);
+	klapan_1_OFF;
+	DelayMillis(inertDelay);
+
+	klapan_4_ON;
+	DelayMillis(cluthDelay);
+	klapan_4_OFF;
+}
 
